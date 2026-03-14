c3e2ccb48232fdaa6f53968eb08050d644116272
jooaquinn <jcenderelli@mail.austral.edu.ar>
2026-03-14 18:21:24 -0300

    Update names.py

--- names.py
+++ names.py
@@ -3,3 +3,12 @@ def names():
     minúsculas, título, mayúsculas y con tabulador.
     """
     pass
+
+    nombre= "joaquin"
+    apellido= "cenderelli"
+    nombre_completo= nombre+""+apellido
+
+    print(nombre_completo.lower())
+    print(nombre_completo.title())
+    print(nombre_completo.upper())
+    print("\t" + nombre_completo)
