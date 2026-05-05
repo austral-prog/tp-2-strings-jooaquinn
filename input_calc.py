5005573b398a0fe1371fc4b82e4912d4fb0e6c24
jooaquinn <jcenderelli@mail.austral.edu.ar>
2026-05-05 10:37:37 -0300

    Update input_calc.py

--- input_calc.py
+++ input_calc.py
@@ -3,13 +3,13 @@ def rectangle():
     el área y el perímetro.
     """
     pass
-
     base = int(input("Base:"))
+    print(f"Base: {base}")
     altura = int(input("Altura:"))
-
+    print(f"Altura: {altura}")
     area = base * altura
     perimetro = (base * 2) + (altura * 2)
 
-
     print(f"Area: {area}")
     print(f"Perimetro: {perimetro}")
+
